b321b557ceaef20e967da934035bfd68f37c3374
Ornela Anahi Lugo Aquino <nelladelugo2@gmail.com>
2022-04-11 19:10:51 -0300

    Servidor con express

--- index.js
+++ index.js
@@ -1,139 +1,103 @@
 const { Console } = require('console')
+
 const fs = require('fs')
+
+const express = require('express')
+
+const app = express()
+
+const PORT = process.env.PORT || 8080
+
+const path = require('path')
+const res = require('express/lib/response')
+
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * ((max + 1) - min)) + min;
+}
 
 class Contenedor{
   constructor(name){
     this.name = name
   }
-  save(obj){
-    fs.readFile(`./${this.name}`,{encoding:'utf-8'},(err,data) => {
+
+  async getData(){
+    const data = await fs.promises.readFile(`./${this.name}`,'utf-8',function (err,data){
+      if(err)throw err
+      const json = JSON.parse(data)
+      return json
+    })
+    return JSON.parse(data)
+  }
+  async save(obj){
+    const file = await this.getData()
+    const id = file.length + 1
+    obj.id = id
+    file.push(obj)
+    fs.writeFile(`./${this.name}`, JSON.stringify(file), function(err){
+      if (err)throw err
+    })
+    console.log(id)
+  }
+  
+  async getAll(){
+    const products = await this.getData()
+    return (products)
+  }
+  async deleteById(deleteId){
+    const file = await this.getData()
+    const itemElim = file.filter((elemento) => elemento.id !== deleteId)
+    const resto = itemElim.map(ele=>{
+      if(ele.id>deleteId){
+        ele.id = ele.id - 1
+      }
+      return ele
+    })
+      fs.writeFile(`./${this.name}`, JSON.stringify(resto), 'utf-8',(err)=>{
+          if(err) throw err
+          console.log('element '+ deleteId + ' delete')
+        })
+  }
+    
+  deleteAll(){
+    fs.writeFile(`./${this.name}`, JSON.stringify([]), 'utf-8',(err)=>{
       if(err){
-        obj.id=1
-        let array=[obj]
-        fs.writeFile(`./${this.name}`, JSON.stringify(array), 'utf-8',(err)=>{
-          if(err){
-            console.error('Error en la escritura de '+ this.name)
-          }
-          else{
-            console.log('Objeto agregado correctamente desde write')
-          }
-        })
-      }else{
-        let array = JSON.parse(data)
-        obj.id=array.length + 1;
-        array.push(obj)
-        fs.writeFile(`./${this.name}`, JSON.stringify(array), 'utf-8', (err)=>{
-          if(err){
-            console.log('Ocurrio un error al escribir el archivo')
-          }
-          else{
-            console.log('Objeto agregado correctamente')
-          }
-        })
+        console.error('Error en la escritura de '+ this.name)
+      }
+      else{
+        console.log('Archivo con array vacio')
       }
     })
   }
-  async getById(id){
-    let getId = await fs.promises.readFile(`./${this.name}`,{encoding:'utf-8'})
-    // const arrayProductos = JSON.parse(getId)
-    return getId
-    // const bsqId = arrayProductos.find(product => product.id === id)
-    // if(bsqId===undefined){
-    //   return null
-    // }
-    // else{
-    //   return bsqId
-    // }
-    // .then((data)=>{
-    //   try{
-    //     console.log(data)
-    //   }catch{
-    //     console.log('catch más exagerado')
-    //   }
-    // }).catch((err)=>{
-    //   throw err
-    // })
-    // fs.readFile(`./${this.name}`,{encoding:'utf-8'},(err,data) => {
-    //   if(err){
-    //     console.error('ocurrio un error')
-    //   }
-    //   else{
-    //     let array = JSON.parse(data)
-    //     let bsqId = 
-
-    //     if(bsqId === undefined){
-    //       return null
-    //     }
-    //     else{
-    //       return (bsqId)
-    //     }
-    //   }
-    // })
-  }
-  // const funcion = async () => {
-  //   let read= await fs.promises.readFile('./text3.txt','utf-8')
-  //   return(read)
-  // }
-  // getAll(){
-  //   fs.readFile(`./${this.name}`,{encoding:'utf-8'},(err,data) => {
-  //     if(err){
-  //       console.log('hubo un error')
-  //     }
-  //     else{
-  //       console.log(JSON.parse(data))
-  //     }
-  //   })
-  // }
-  // deleteById(deleteId){
-  //   fs.readFile(`./${this.name}`,{encoding:'utf-8'},(err,data) => {
-  //     if(err){
-  //       console.log('hubo un error')
-  //     }
-  //     else{
-  //       let array = JSON.parse(data)
-  //       const itemElimin = array.filter((elemento) => elemento.id !== deleteId)
-  //       const resto = itemElimin.map(ele=>{
-  //         if(ele.id>deleteId){
-  //           ele.id = ele.id - 1
-  //         }
-  //         return ele
-  //       })
-  //       fs.writeFile(`./${this.name}`, JSON.stringify(resto), 'utf-8',(err)=>{
-  //         if(err){
-  //           console.error('Error en la escritura de '+ this.name)
-  //         }
-  //         else{
-  //           console.log('Objeto borrado correctamente')
-  //         }
-  //       })
-  //     }
-  //   })
-  // }
-  // deleteAll(){
-  //   fs.writeFile(`./${this.name}`, JSON.stringify([]), 'utf-8',(err)=>{
-  //     if(err){
-  //       console.error('Error en la escritura de '+ this.name)
-  //     }
-  //     else{
-  //       console.log('Archivo con array vacio')
-  //     }
-  //   })
-  // }
 }
 
 
-
-class Producto{
-  constructor(title,price,thumbnail){
-    this.title = title
-    this.price = price
-    this.thumbnail = thumbnail
-  }
-}
 let file1 = new Contenedor('text.json')
-// file1.save(new Producto('Taza estilo Mafalda', 450, 'https://www.quevainatazas.com/wp-content/uploads/2019/05/mafalda8.jpg'))
-
-console.log(file1.getById(2))
 // file1.getAll()
 // file1.deleteById(2)
 // file1.deleteAll()
+
+app.listen(PORT, ()=>{
+  console.log('server run on port 8080')
+})
+app.get('/', (req,res)=>{
+  res.send('Pagina de inicio')
+})
+
+const productos = async()=>{
+  const TLProd = await file1.getAll()
+  app.get('/productos', (req,res)=>{
+    res.send(TLProd)
+  })
+}
+productos()
+const productosRandom = async ()=>{
+  const TLProd = await file1.getAll()
+  const mayor = TLProd.length
+  app.get('/productosRandom', (req, res)=>{
+    numeroRandom = getRandomInt(1, mayor)
+    let bsqDeNR = TLProd.find(product => product.id == numeroRandom)
+    res.send(bsqDeNR)
+  })
+}
+productosRandom()
